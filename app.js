b276e1c58c2b198646f3a1562def79825517848e
EC2 Default User <ec2-user@ip-172-31-16-208.ec2.internal>
2026-06-18 17:40:03 +0000

    fix test mismatch

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('CloudShop API LIVE UPDATE');
+    res.send('CloudShop API Running');
 });
 
 if (require.main === module) {
