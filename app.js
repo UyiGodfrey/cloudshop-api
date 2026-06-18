9b662b1f06ce6549fa89c4cc887ddbe98c47a1d2
EC2 Default User <ec2-user@ip-172-31-16-208.ec2.internal>
2026-06-18 17:21:58 +0000

    test auto deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('CloudShop API Running');
+    res.send('CloudShop API UPDATED');
 });
 
 if (require.main === module) {
